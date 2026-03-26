57753f7d7445366a3d8aa9e21f444a956789c563
fio <the_oronco@posteo.net>
2026-03-26 22:51:46 +0100

    added doors that teleport (screen transition still TODO)

---
+++ entities/door/door.gd
@@ -0,0 +1,21 @@
+@tool
+extends Node3D
+
+var _target: Marker3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Engine.is_editor_hint(): return
+	_target = get_children().filter(func(c):return c is Marker3D)[0]
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if !get_children().any(func(c):return c is Marker3D): return ["Add a Marker3D Node as child to define the target of this door"]
+	return []
+
+
+func _on_player_interaction(player: CharacterBody3D) -> void:
+	player.global_position = _target.global_position
+
+
+func _on_editor_state_changed() -> void:
+	update_configuration_warnings()
--- entities/interactable/interactable.gd
+++ entities/interactable/interactable.gd
@@ -2,17 +2,22 @@ class_name Interactible
 extends Area3D
 
 var interactable := false
+var _player: CharacterBody3D = null
 signal interacted
+signal player_interacted(player: CharacterBody3D)
 
-func _on_body_entered(_body: Node3D) -> void:
+func _on_body_entered(body: Node3D) -> void:
 	interactable = true
+	_player = body
 	print("Entered")
 
 func _on_body_exited(_body: Node3D) -> void:
 	interactable = false
+	_player = null
 	print("Exited")
 
 func _input(event: InputEvent) -> void:
 	if interactable and event.is_action_pressed("player_interact"):
 		print("interaction")
 		interacted.emit()
+		player_interacted.emit(_player)
