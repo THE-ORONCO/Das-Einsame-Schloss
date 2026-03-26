23d510d5c23a9131592bdc1e0424e6c4c99331e0
fio <the_oronco@posteo.net>
2026-03-26 14:55:05 +0100

    changing pin variables now affect both in editor and in game values

---
+++ entities/lock/lock.gd
@@ -0,0 +1,7 @@
+@tool
+extends Node2D
+
+
+
+@onready var body: StaticBody2D = %body
+@onready var shape: CollisionPolygon2D = %shape
--- entities/lock/pin/pin.gd
+++ entities/lock/pin/pin.gd
@@ -2,24 +2,38 @@
 class_name Pin 
 extends Node2D
 
-@onready var rest_position: StaticBody2D = %RestPosition
-@onready var groove: GrooveJoint2D = %groove
-@onready var spring: DampedSpringJoint2D = %spring
+@onready var rest_position: StaticBody2D = $RestPosition
+@onready var groove: GrooveJoint2D = $groove
+@onready var spring: DampedSpringJoint2D = $spring
+@onready var pin: RigidBody2D = $Pin
 
-@export_range(0, 4)
-var resistance: float = 0.5:
-	set(val): set_deferred("spring.damping", val)
-	get: return spring.damping
 @export_range(0,64)
 var spring_strength: float = 32:
-	set(val): set_deferred("spring.stiffness", val)
-	get: return spring.stiffness
-	
+	set(val):
+		spring_strength = val
+		if groove != null : 
+			spring.stiffness = spring_strength
 @export_range(-20, 20)
 var move_upper: float = 0:
-	set(val): set_deferred("groove.position.y",  val)
-	get: return groove.position.y
+	set(val): 
+		move_upper = val
+		if groove != null && spring != null: 
+			groove.position = Vector2(0, move_upper)
+			spring.position = Vector2(0, move_upper)
+			pin.position = Vector2(0, move_upper)
+			rest_position.position = Vector2(0, move_upper)
+			
 @export_range(0, 64)
 var move_lower: float =  24:
-	set(val): set_deferred("groove.length",val) 
-	get: return groove.length
+	set(val): 
+		move_lower = val
+		if groove != null : 
+			groove.length = move_lower 
+			spring.length = move_lower
+	
+func _ready() -> void:
+	if Engine.is_editor_hint(): return 
+	
+	spring_strength = spring_strength
+	move_lower = move_lower
+	move_upper = move_upper
