3580fc6b986e3910a064d3ae32e43abda3acf76d
fio <the_oronco@posteo.net>
2026-03-27 14:10:38 +0100

    objects can now be haunted after a certain point in the story by setting a dialogue variable

--- entities/interactable/interactable.gd
+++ entities/interactable/interactable.gd
@@ -3,11 +3,20 @@ extends Area3D
 
 var interactable := false
 var _player: CharacterBody3D = null
+var was_actiated_later: bool:
+	get:
+		var later = SproutyDialogs.Variables.get_variable("activate_objects")
+		return later != null && later == true
 signal interacted
 signal player_interacted(player: CharacterBody3D)
+const SPOOK_POINT = preload("uid://b1yawdyvaadun")
+
+
+@export
+var activate_later: bool = false
 
 func _on_body_entered(body: Node3D) -> void:
-	interactable = true
+	interactable = true if !activate_later else was_actiated_later
 	_player = body
 	print("Entered")
 
@@ -21,3 +30,11 @@ func _input(event: InputEvent) -> void:
 		print("interaction")		
 		interacted.emit()
 		player_interacted.emit(_player)
+
+var haunted: bool = false
+func _physics_process(delta: float) -> void:
+	if activate_later && was_actiated_later && not haunted:
+		var haunt: SpookPoint = SPOOK_POINT.instantiate()
+		self.add_child(haunt)
+		haunt.global_position = self.global_position
+		haunted = true
