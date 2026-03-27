c38e96d20e092681d3b5198c878c31931b0bb031
fio <the_oronco@posteo.net>
2026-03-27 15:16:54 +0100

    interactables can now trigger on contact

--- entities/interactable/interactable.gd
+++ entities/interactable/interactable.gd
@@ -2,6 +2,7 @@ class_name Interactible
 extends Area3D
 
 var interactable := false
+
 var _player: CharacterBody3D = null
 var was_actiated_later: bool:
 	get:
@@ -14,11 +15,15 @@ const SPOOK_POINT = preload("uid://b1yawdyvaadun")
 
 @export
 var activate_later: bool = false
+@export
+var trigger_on_contact: bool = false
 
 func _on_body_entered(body: Node3D) -> void:
 	interactable = true if !activate_later else was_actiated_later
 	_player = body
 	print("Entered")
+	if interactable && trigger_on_contact:
+		_trigger_interaction()
 
 func _on_body_exited(_body: Node3D) -> void:
 	interactable = false
@@ -27,9 +32,12 @@ func _on_body_exited(_body: Node3D) -> void:
 
 func _input(event: InputEvent) -> void:
 	if interactable and event.is_action_pressed("player_interact"):
-		print("interaction")		
-		interacted.emit()
-		player_interacted.emit(_player)
+		_trigger_interaction()
+
+func _trigger_interaction() -> void:
+	print("interaction")
+	interacted.emit()
+	player_interacted.emit(_player)
 
 var haunted: bool = false
 func _physics_process(delta: float) -> void:
