7ee71e62f48095dfa8cd2c62baafeb18ad93e53f
fio <the_oronco@posteo.net>
2026-03-27 09:34:41 +0100

    added direction keeping when waling in a direction

--- entities/character/character.gd
+++ entities/character/character.gd
@@ -9,6 +9,16 @@ const JUMP_VELOCITY = 4.5
 @onready var light: SpotLight3D = %light
 @onready var light_rot: float = %light.global_rotation.y
 
+enum DIR{
+	U,
+	D,
+	L,
+	R
+}
+
+var _current_dir: DIR = DIR.D
+var _moving: bool = false
+
 @export
 var spook_increments: Array[float] = [1000000, 8, 6, 3, 1, .5]
 
@@ -18,12 +28,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("player_left", "player_right", "player_up", "player_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -34,31 +38,24 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	_animate_sprite()
+	_orientate()
+	if _moving: _move_animate()
+	else: _idle_animate()
+	_point_light()
 	_update_emp()
 
 
-func _animate_sprite() -> void:
-	if velocity.length_squared() < 0.1:
-		animation.play("idle")
-		light.rotation.y = light_rot
-		return
+func _orientate() -> void:
+
+	_moving = velocity.length_squared() > 0.1
 	
 	var dir := velocity.normalized()
 	if abs(dir.x) > abs(dir.z):
-		if dir.x > 0: 
-			animation.play("right")
-			light.global_rotation.y = light_rot + PI/2
-		if dir.x < 0: 
-			animation.play("left")
-			light.global_rotation.y = light_rot - PI/2
+		if dir.x > 0: _current_dir = DIR.R
+		if dir.x < 0: _current_dir = DIR.L
 	elif abs(dir.x) < abs(dir.z) :
-		if dir.z > 0: 
-			animation.play("down")
-			light.global_rotation.y = light_rot
-		if dir.z < 0:
-			animation.play("up")
-			light.global_rotation.y = light_rot + PI
+		if dir.z > 0: _current_dir = DIR.D
+		if dir.z < 0: _current_dir = DIR.U
 
 
 func _update_emp() -> void:
@@ -70,5 +67,24 @@ func _update_emp() -> void:
 			var random_offset := randf() *3 - 1
 			emp_tex.current_frame = clampf(i + random_offset, 0, max_spook - 1) 
 
-	
-	
+
+func _idle_animate() -> void:
+	match _current_dir:
+		DIR.U: animation.play("idle_up")
+		DIR.D: animation.play("idle_down")
+		DIR.L: animation.play("idle_left")
+		DIR.R: animation.play("idle_right")
+
+func _move_animate() -> void:
+	match _current_dir:
+		DIR.U: animation.play("up")
+		DIR.D: animation.play("down")
+		DIR.L: animation.play("left")
+		DIR.R: animation.play("right")
+
+func _point_light() -> void:
+	match _current_dir:
+		DIR.U: light.global_rotation.y = light_rot + PI
+		DIR.D: light.global_rotation.y = light_rot
+		DIR.L: light.global_rotation.y = light_rot - PI/2
+		DIR.R: light.global_rotation.y = light_rot + PI/2
